28f5d32b416fc56a07a66c95adeb176fa1de32e2
Ruben Rodrigo <ruben.chani@tecsup.edu.pe>
2021-02-07 12:51:37 -0500

    configuracion base de datos

--- tienda/settings.py
+++ tienda/settings.py
@@ -74,22 +74,22 @@ WSGI_APPLICATION = 'tienda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'caracruz',
-        'USER': 'rodrigo',
-        'PASSWORD': 'nativa-caracruz',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'caracruz',
+#         'USER': 'rodrigo',
+#         'PASSWORD': 'nativa-caracruz',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
